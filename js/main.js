5b47941d5fd725f1c2b008f54c1b803c730c0dcb
Yerin Jung <yell_10@naver.com>
2021-11-05 16:48:33 +0900

    구글 로그인 기능 추가

--- js/main.js
+++ js/main.js
@@ -20,7 +20,13 @@
 		});
 
 	};
-
+	function onSignIn(googleUser) {
+		var profile = googleUser.getBasicProfile();
+		console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+		console.log('Name: ' + profile.getName());
+		console.log('Image URL: ' + profile.getImageUrl());
+		console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+	  }
 
 	var offcanvasMenu = function() {
 
@@ -207,6 +213,7 @@
 		loaderPage();
 		counterWayPoint();
 	});
+
 
 
 }());
